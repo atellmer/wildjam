9458d3e4227ea3eae749a63209063d8b5e7f3682
atellmer <atellmer@gmail.com>
2017-05-22 18:11:05 +0500

    new build

--- build/components/modal/modal.component.js
+++ build/components/modal/modal.component.js
@@ -3,107 +3,16 @@
 	document.addEventListener('DOMContentLoaded', ready);
 
 	function ready() {
-		modalInit();
-	}
+		// инициализируем модальные окна
+		wjModal.init();
 
-	function modalInit() {
-		var btns = document.querySelectorAll('[data-modal-trigger]'),
-				overlay = document.querySelector('.modals__overlay'),
-				hammerOverlay,
-				hammerCloseBtn,
-				i;
+		/*
+		открыть программно окно
+		wjModal.open(id), где id - это номер окна в атрибуте data-modal-target
 
-		for (i = 0; i < btns.length; i++) {
-			new Hammer(btns[i]).on('tap', clickModalBtnsHandler);
-		}
-
-		function clickModalBtnsHandler(ev) {
-			var id, target, closeBtn, i;
-
-			if (ev.target.hasAttribute('data-modal-trigger')) {
-				id = ev.target.getAttribute('data-modal-trigger');
-			} else {
-				id = ev.target.closest('[data-modal-trigger]').getAttribute('data-modal-trigger');
-			}
-
-			target = document.querySelector('[data-modal-target="' + id + '"]');
-			closeBtn = document.querySelector('[data-modal-target="' + id + '"] [data-modal-close]');
-
-			hammerOverlay = new Hammer(overlay).on('tap', clickCloseBtnHandler);
-			hammerCloseBtn = new Hammer(closeBtn).on('tap', clickCloseBtnHandler);
-
-			target.style.display = 'block';
-			overlay.style.display = 'block';
-
-			if (target.classList.contains('js-is-close')) {
-				target.classList.remove('js-is-close');
-			}
-
-			if (overlay.classList.contains('js-is-close')) {
-				overlay.classList.remove('js-is-close');
-			}
-
-			if (!target.classList.contains('js-is-open')) {
-				target.classList.add('js-is-open');
-			}
-
-			if (!overlay.classList.contains('js-is-open')) {
-				overlay.classList.add('js-is-open');
-			}
-
-			blockScroll();
-
-			function clickCloseBtnHandler() {
-				if (!target.classList.contains('js-is-close')) {
-					target.classList.add('js-is-close');
-				}
-
-				if (!overlay.classList.contains('js-is-close')) {
-					overlay.classList.add('js-is-close');
-				}
-
-				if (target.classList.contains('js-is-open')) {
-					target.classList.remove('js-is-open');
-				}
-
-				if (overlay.classList.contains('js-is-open')) {
-					overlay.classList.remove('js-is-open');
-				}
-
-				setTimeout(function() {
-					target.style.display = 'none';
-					overlay.style.display = 'none';
-				}, 500);
-
-				unblockScroll();
-			}
-		}
-
-		function blockScroll() {
-			var body = document.querySelector('body');
-
-			if (isScrollDesktop()) {
-				body.style.paddingRight = '17px';
-			}
-			body.style.overflow = 'hidden';
-		}
-
-		function unblockScroll() {
-			var body = document.querySelector('body');
-
-			body.style.overflow = 'auto';
-			if (isScrollDesktop()) {
-				body.style.paddingRight = '0px';
-			}
-		}
-
-		function isScrollDesktop() {
-			if (window.innerWidth !== document.documentElement.clientWidth) {
-				return true;
-			}
-
-			return false;
-		}
+		закрыть программно окно
+		wjModal.close(id), где id - это номер окна в атрибуте data-modal-target
+		*/
 	}
 
 })();
