4e78c3802cb9cd171ca5b0b6dba47be093c0c4e1
atellmer <atellmer@gmail.com>
2017-06-15 15:14:30 +0500

    new build

--- components/case-card/case-card.component.js
+++ components/case-card/case-card.component.js
@@ -2,55 +2,41 @@
 (function() {
 	'use strict';
 
-	window.addEventListener('load', ready);
+	document.addEventListener('DOMContentLoaded', ready);
 
 	function ready() {
-		var slider = $('[data-case-video-slider]');
+		sliderInit('[data-case-video-slider]');
+	}
 
-		if (slider) {
-			slider.slick({
-				infinite: true,
-				speed: 300,
-				autoplay: true,
-				autoplaySpeed: 5000,
-				arrows: false,
-				pauseOnHover: false,
-				dots: true
-			});
+	function sliderInit(selector) {
+		waitImages(selector);
+	}
 
-			renderDotsSign();
+	function waitImages(selector) {
+		$(selector).waitForImages(waitImagesHandler);
+
+		function waitImagesHandler() {
+			showSlider(selector);
 		}
 	}
 
-	function renderDotsSign() {
-		var root = document.querySelector('[data-case-video-slider]'),
-				dots, list, count,
-				sign = ' рекламных роликов: ';
+	function showSlider(selector) {
+		var root = document.querySelector(selector),
+				options = {
+					infinite: true,
+					speed: 300,
+					autoplay: true,
+					autoplaySpeed: 5000,
+					arrows: false,
+					pauseOnHover: false,
+					dots: true
+				};
 
-		if (root) {
-			list = root.querySelectorAll('[data-case-video-item]');
-			dots = root.querySelector('.slick-dots'),
-			count = list.length - 2;
+		$(selector).slick(options);
 
-			count < 5 ? sign = ' рекламных ролика: ' : sign = sign;
-
-			if (dots) {
-				dots.insertAdjacentHTML('beforebegin', render({
-					sign: count + sign
-				}));
-			}
-
-			root.classList.contains('js-is-hide') ? root.classList.remove('js-is-hide') : false;
-		}
-
-		function render(props) {
-			return [
-				'<span class="case-card__dots-sign">',
-					props.sign,
-				'</span>'
-			].join('');
-		}
+		root.classList.contains('js-is-hide')
+		? root.classList.remove('js-is-hide')
+		: false;
 	}
-
 
 })();
