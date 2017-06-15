30cb2efd579d00dee0d81b0d5a974acec68c5b98
atellmer <atellmer@gmail.com>
2017-06-15 16:49:12 +0500

    new build

--- build/shared/flux/actions/order.js
+++ build/shared/flux/actions/order.js
@@ -224,7 +224,7 @@
 
 		function renderButtonLoader(text) {
 			return [
-				'<div class="loader">',
+				'<div class="wj-loader wj-loader--small wj-loader--white">',
 					'<svg class="circular" viewbox="25 25 50 50">',
 						'<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="4" stroke-miterlimit="10"/>',
 					'</svg>',
